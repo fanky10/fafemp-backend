5324d703bf2d7794e97b7147c8af463bb7e10e00
Facundo Scoccia <fanky10@gmail.com>
2013-01-02 21:26:20 -0300

    admin organizado y andando

---
+++ dev/web/admin/admin_panel.php
@@ -0,0 +1,23 @@
+<?php
+session_start(); 
+    $msg="Bienvenido ".$_SESSION['user'];
+?>
+
+<html>
+<head><title>User Panel</title></head>
+<body>
+<p> <?php echo $msg; ?> </p>
+
+<form id="form2" name="form2" method="post" action="cambio_pass_form.php">
+  <input type="submit" name="cambio_pass" id="logout" value="Cambiar contraseña" />
+</form>
+
+<form id="form2" name="form3" method="post" action="logout.php">
+  <input type="submit" name="logout" id="logout" value="Desconectarme" />
+</form>
+
+<a href="noticias_add_form.php">Cargar nueva noticia!</a>
+
+
+</body>
+</html>
