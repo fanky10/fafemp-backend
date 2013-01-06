c6037f57e2ac39ec4ec28ddb5fe8428b6883f892
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2013-01-06 19:55:44 -0300

    agregado el footer

--- dev/web/footer.php
+++ dev/web/footer.php
@@ -1,57 +1,57 @@
 <!-- Three-up Content Blocks -->
 <div class="content">
 
-	<!-- Footer -->
+    <!-- Footer -->
 
-	<div class="footer footer-line-white">
-		<footer class="row">
-			<div class="twelve columns">
-				<div class="row">
-					<div class="four columns">
-						<h4>Contacto</h4>
-						<p class="last">
-							<img src="images/ico-phone.png" alt="Teléfono" /> Tel: (0341) -
-							555555
-						</p>
-						<p class="last">
-							<img src="images/ico-phone.png" alt="Teléfono" /> Fax: (0341) -
-							555555
-						</p>
-						<p>
-							<img src="images/ico-mail.png" alt="Mail" /> E-mail: <a
-								href="mailto:info@fafemp.org">info@fafemp.org</a>
-						</p>
-						<p class="last">
-							<img src="images/ico-home.png" alt="Dirección" /> 
-							<a href="https://maps.google.com.ar/maps/ms?msid=206810590642046489742.0004d26020155865bcfe4&msa=0&ll=-33.998027,-61.21582&spn=10.121215,20.214844&t=m&output=embed?iframe=true&width=100%&height=100%" rel="prettyPhoto[iframes]">Santa Fe 3100, Rosario - Argentina</a>
-									 
-						</p>
-					</div>
-					<div class="four columns">
-						<h4>Google Maps</h4>
-						<a href="https://maps.google.com.ar/maps/ms?msid=206810590642046489742.0004d26020155865bcfe4&msa=0&ll=-33.998027,-61.21582&spn=10.121215,20.214844&t=m&output=embed?iframe=true&width=100%&height=100%" rel="prettyPhoto[iframes]">
-							<img src="images/google-maps-rosario-argentina.png" alt="Google Maps - Rosario - Argentina" /> 
-						</a>
-					</div>
-					<div class="four columns">
-						<h4>Contacto Social</h4>
-						<ul class="social-box link-list">
-							<li><a href="#" class="facebook">Facebook</a></li>
-							<li><a href="#" class="twitter">Twitter</a></li>
-							<li><a href="#" class="linkedin">LinkedIn</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="row copyright">
-					<div class="twelve columns">
-						<hr class="footer-line" />
-						<p class="last">FAFEMP © 2013 - Foro Argentino de Facultades y
-							Escuelas de Medicina Públicas.</p>
-					</div>
-				</div>
-			</div>
-		</footer>
-	</div>
-	<!-- Included JS Files (Uncompressed) -->
-        
+    <div class="footer footer-line-white">
+        <footer class="row">
+            <div class="twelve columns">
+                <div class="row">
+                    <div class="four columns">
+                        <h4>Contacto</h4>
+                        <p class="last">
+                            <img src="images/ico-phone.png" alt="Teléfono" /> Tel: (0341) -
+                            555555
+                        </p>
+                        <p class="last">
+                            <img src="images/ico-phone.png" alt="Teléfono" /> Fax: (0341) -
+                            555555
+                        </p>
+                        <p>
+                            <img src="images/ico-mail.png" alt="Mail" /> E-mail: <a
+                                href="mailto:info@fafemp.org">info@fafemp.org</a>
+                        </p>
+                        <p class="last">
+                            <img src="images/ico-home.png" alt="Dirección" /> 
+                            <a href="https://maps.google.com.ar/maps/ms?msid=206810590642046489742.0004d26020155865bcfe4&msa=0&ll=-33.998027,-61.21582&spn=10.121215,20.214844&t=m&output=embed?iframe=true&width=100%&height=100%" rel="prettyPhoto[iframes]">Unidades académicas participantes</a>
+
+                        </p>
+                    </div>
+                    <div class="four columns" hidden="true">
+                        <h4>Google Maps</h4>
+                        <a href="https://maps.google.com.ar/maps/ms?msid=206810590642046489742.0004d26020155865bcfe4&msa=0&ll=-33.998027,-61.21582&spn=10.121215,20.214844&t=m&output=embed?iframe=true&width=100%&height=100%" rel="prettyPhoto[iframes]">
+                            <img src="images/google-maps-rosario-argentina.png" alt="Google Maps - Rosario - Argentina" /> 
+                        </a>
+                    </div>
+                    <div class="four columns">
+                        <h4>Contacto Social</h4>
+                        <ul class="social-box link-list">
+                            <li><a href="#" class="facebook">Facebook</a></li>
+                            <li><a href="#" class="twitter">Twitter</a></li>
+                            <li><a href="#" class="linkedin">LinkedIn</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row copyright">
+                    <div class="twelve columns">
+                        <hr class="footer-line" />
+                        <p class="last">FAFEMP © 2013 - Foro Argentino de Facultades y
+                            Escuelas de Medicina Públicas.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- Included JS Files (Uncompressed) -->
+
 </div>
