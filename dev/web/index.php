362ad44d544fd1a8e05ffe7f02c1f9a7b074db5c
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2013-01-08 14:11:49 -0300

    corregido header y pag de error

--- dev/web/index.php
+++ dev/web/index.php
@@ -31,23 +31,7 @@
     <body>
 
         <!-- Header and Nav -->
-        <div class="logo">
-            <div class="row">
-                <div class="one columns isotipo">
-                    <a href="index.html" title="Foro Argentino de Facultades y Escuelas de Medicina Públicas"><img src="images/logo-foro-argentino-de-facultades-y-escuelas-de-medicina-publicas.png" /></a></div>
-                <div class="eight columns">
-                    <h2 class="slogan">Contribuyendo a formar los profesionales de la salud que el país necesita</h2>
-                    <h1 class="logotipo">Foro Argentino de Facultades y Escuelas de Medicina Públicas</h1>
-                </div>
-                <div class="three columns">
-                    <ul class="social-box link-list right">
-                        <li><a href="#" class="facebook">Facebook</a></li>
-                        <li><a href="#" class="twitter">Twitter</a></li>
-                        <li><a href="#" class="linkedin">LinkedIn</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <?php include_once 'header.php'; ?>
 
         <!-- End Header and Nav -->
         <?php include_once 'menu_header.php'; ?>
