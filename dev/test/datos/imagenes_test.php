1240f7493586d232658e58e9af197ccd0f6d07bb
Facundo Scoccia <fanky10@gmail.com>
2013-01-21 13:59:49 -0300

    super class que maneja cuando inicia y termina c/ unit case (: casi como annotation @Transaction ejeje

--- dev/test/datos/imagenes_test.php
+++ dev/test/datos/imagenes_test.php
@@ -1,48 +1,36 @@
 <?php
 
 require_once ('../initTesting.php');
-require_once (TEST_ROOT_DIR . '/simpletest/autorun.php');
 require_once (WEB_ROOT_DIR . '/datos/imagenes.php');
 require_once (WEB_ROOT_DIR . '/datos/noticias.php');
 require_once (WEB_ROOT_DIR . '/datos/data.php');
 require_once (WEB_ROOT_DIR . '/entidades/imagen.php');
 require_once (WEB_ROOT_DIR . '/entidades/noticia.php');
+require_once ('db_isolated_test_case.php');
 
-class ImagenesTest extends UnitTestCase {
+class ImagenesTest extends DatabaseIsolatedTestCase {
 
     private $imagenesRepository;
     private $noticiasRepository;
-    private $dataSource;
+
     public function __construct() {
         parent::__construct("Imagenes Test");
         $this->imagenesRepository = new DataImagenes();
         $this->noticiasRepository = new DataNoticias();
-        $this->dataSource = MysqlDataSource::getInstance();
-        
-    }
-    
-    //isolate each test (:
-    function setUp() {
-        $this->dataSource->startTransaction();
-    }
-    
-    function tearDown() {
-        $this->dataSource->rollbackTransaction();
-        $this->dataSource->closeConnection();
     }
 
     function testAddImage() {
         $idNoticia = $this->agregaNoticia();
-        
+
         $imagen = new Imagen();
         $imagen->setNombre("pepe.png");
         $imagen->setPath("/pepe/");
 
         $idImagen = $this->imagenesRepository->addImagenNoticia($imagen, $idNoticia);
         $this->assertTrue(isset($idImagen));
-        
     }
-    private function agregaNoticia(){
+
+    private function agregaNoticia() {
         $noticia = new Noticia();
         $noticia->setCuerpo("Noticia con imagen!");
         $noticia->setTitulo("una noticia con imagen");
--- dev/test/datos/noticias_test.php
+++ dev/test/datos/noticias_test.php
@@ -1,29 +1,20 @@
 <?php
 
 require_once ('../initTesting.php');
-require_once (TEST_ROOT_DIR . '/simpletest/autorun.php');
 require_once (WEB_ROOT_DIR . '/datos/noticias.php');
 require_once (WEB_ROOT_DIR . '/datos/data.php');
 require_once (WEB_ROOT_DIR . '/entidades/noticia.php');
+require_once ('db_isolated_test_case.php');
 
-class NoticiasTest extends UnitTestCase {
+class NoticiasTest extends DatabaseIsolatedTestCase {
 
     private $noticiasRepository;
-    private $dataSource;
+    
     public function __construct() {
         parent::__construct("Imagenes Test");
         $this->noticiasRepository = new DataNoticias();
-        $this->dataSource = MysqlDataSource::getInstance();
     }
-    function setUp() {
-        $this->dataSource->startTransaction();
-    }
-    //isolate each test (:
-    function tearDown() {
-        $this->dataSource->rollbackTransaction();
-        $this->dataSource->closeConnection();
-    }
-
+    
     function testAddNoticia() {
         $idNoticia = $this->agregaNoticia();
         $this->assertTrue(isset($idNoticia));
@@ -41,7 +32,7 @@ class NoticiasTest extends UnitTestCase {
         $this->assertTrue(isset($vNoticias) && count($vNoticias)==$limit);
     }
     
-    function agregaNoticia(){
+    private function agregaNoticia(){
         $noticia = new Noticia();
         $noticia->setCuerpo("Esto es un cuerpo");
         $noticia->setTitulo("re title!");
