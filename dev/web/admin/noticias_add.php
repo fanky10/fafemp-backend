06eac53def1fa2f23f93ceae568c93734c261c75
Facundo Scoccia <fanky10@gmail.com>
2013-01-23 13:55:11 -0300

    multi-imagenes preview y noticia andando

--- dev/web/admin/noticias_add.php
+++ dev/web/admin/noticias_add.php
@@ -17,37 +17,69 @@ var_dump($oNoticia);
 echo"--> ";
 ?>
 <!DOCTYPE html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js" lang="en">
-    <!--<![endif]-->
-    <head>
-        <meta charset="utf-8" />
 
-        <!-- Set the viewport width to device width for mobile -->
-        <meta name="viewport" content="width=device-width" />
+    <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+    <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+    <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+    <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+    <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+        <head>
+            <meta charset="utf-8" />
 
-        <title>Foro Argentino de Facultades y Escuelas de Medicina
-            Públicas | ADMIN SITE</title>
+            <!-- Set the viewport width to device width for mobile -->
+            <meta name="viewport" content="width=device-width" />
 
-        <!-- Included CSS Files (Uncompressed) -->
-        <!--
-          <link rel="stylesheet" href="stylesheets/foundation.css">
-        -->
+            <title>Foro Argentino de Facultades y Escuelas de Medicina Públicas | Home</title>
 
-        <!-- Included CSS Files (Compressed) -->
-        <link rel="stylesheet" href="../stylesheets/foundation.css">
-        <link rel="stylesheet" href="../stylesheets/app.css">
-        <link rel="stylesheet" href="../stylesheets/prettyPhoto.css">
+            <!-- Included CSS Files (Uncompressed) -->
+            <!--
+            <link rel="stylesheet" href="stylesheets/foundation.css">
+            -->
 
-        <!-- Author -->
-        <link type="text/plain" rel="author" href="../humans.txt" />
+            <!-- Included CSS Files (Compressed) -->
+            <link rel="stylesheet" href="../stylesheets/foundation.css">
+            <link rel="stylesheet" href="../stylesheets/app.css">
+            <link rel="stylesheet" href="../stylesheets/prettyPhoto.css">
+            <link rel="stylesheet" href="../stylesheets/flexslider.css">
 
-        <script src="../javascripts/modernizr.foundation.js"></script>
-    </head>
+            <!-- Author -->
+            <link type="text/plain" rel="author" href="humans.txt" />
+
+            <script src="../javascripts/modernizr.foundation.js"></script>
+
+            <!-- Syntax Highlighter -->
+            <script src="../javascripts/modernizr.js"></script>
+            <!-- Included JS Files (Compressed) -->
+            <script src="../javascripts/jquery.js"></script>
+            <script src="../javascripts/foundation.min.js"></script>
+
+            <!-- Initialize JS Plugins -->
+            <script src="../javascripts/jquery.prettyPhoto.js"></script>
+            <script src="../javascripts/jquery_validate.js"></script>
+            <script src="../javascripts/app.js"></script>
+            <script src="../javascripts/init.js"></script>
+
+            <!-- jQuery -->
+
+            <!-- FlexSlider -->
+            <script defer src="../javascripts/jquery.flexslider.js"></script>
+            <script defer src="../javascripts/jquery.flexslider-min.js"></script>
+
+            <script type="text/javascript">
+                $(function(){
+                    SyntaxHighlighter.all();
+                });
+                $(window).load(function(){
+                    $('.flexslider').flexslider({
+                        animation: "slide",
+                        start: function(slider){
+                            $('body').removeClass('loading');
+                        }
+                    });
+                });
+            </script>
+
+        </head>
     <body>
 
         <?php
