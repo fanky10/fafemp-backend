460013cd684432dfbedde1ae8aefa8ee719d4782
Facundo Scoccia <fanky10@gmail.com>
2013-01-24 13:20:59 -0300

    version final de dragme, ya anda como quiero

--- dev/test/draggable/dragme.php
+++ dev/test/draggable/dragme.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>jQuery UI Sortable - Default functionality</title>
+        <title>Draggin and sorting!</title>
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.0/themes/base/jquery-ui.css" />
         <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
         <script src="http://code.jquery.com/ui/1.10.0/jquery-ui.js"></script>
@@ -13,33 +13,54 @@
             #sortable li span { position: absolute; margin-left: -1.3em; }
         </style>
         <script>
+            
             $(function() {
+                function createObject(id, position) {
+
+                    return {
+                        "id": id,
+                        "position": position
+                    }
+
+                }
                 $( "#sortable" ).sortable({
                     update: function(event, ui) {
-                        var listOrder = $(this).sortable('toArray').toString();
-                        $("#response").html("my selection list "+listOrder);
-                        $.post("ajax.php",listOrder);
+                        var result = [];//new Array();
+                        $("#sortable li").each(function(idx, item){
+                            var id = $(item).attr('id');
+                            var oRow = createObject(id,idx);
+                            result.push(oRow);
+                            
+                        });
+                        //once we have the result let's show it!!
+                        var jsonResult = JSON.stringify(result);
+                        //once we know it works let's send it!!
+                        $.post(
+                        "json_process.php",
+                        {jsonObject: jsonResult},
+                        function(data){
+                            $("#response").html(data);
+                        }
+                    );
+                        
                     }
-                    
-                    
                 });
                 $( "#sortable" ).disableSelection();
-                
             });
         </script>
     </head>
     <body>
         <ul id="sortable">
             <?php
-                $i = 0;
-                while ($i<10){
-                    $i++;
-                    echo '<li id="'.$i.'" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item '.$i.'</li>';
-                }
+            $i = 0;
+            while ($i < 5) {
+                $i++;
+                echo '<li id="' . $i . '" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item ' . $i . '</li>';
+            }
             ?>
         </ul>
         <div id="response">
-            
+
         </div>
     </body>
 </html>
