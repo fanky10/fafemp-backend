9a5bb6a320346c54ea82dc104fb966b919e948bc
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2013-02-16 12:09:06 -0300

    create_schema editado para reuniones

--- dev/web/db/create_schema.sql
+++ dev/web/db/create_schema.sql
@@ -7,9 +7,8 @@ drop database if exists fafemp_web;
 create database fafemp_web;
 grant all privileges on fafemp_web.* to fafemp_root@'localhost' identified by 'root';
 connect fafemp_web;
-/**
-* reglas de negocio a aplicar en las noticias, la noticia tiene un titulo, una fecha-hora, un cuerpo y (al menos por ahora) una sola imagen.
-*/
+
+/* NOTICIAS */
 DROP TABLE IF EXISTS noticias;
 
 CREATE TABLE noticias(
@@ -38,10 +37,45 @@ CREATE TABLE imagenes_noticia (
 
 ALTER TABLE imagenes_noticia ADD CONSTRAINT `FK_imagenes_noticia_id_1` FOREIGN KEY (`imagen_noticia_id`) REFERENCES `noticias` (`noticia_id`);
 
+/* FIN NOTICIAS */
 
+
+/* REUNIONES */
+DROP TABLE IF EXISTS reuniones;
+
+CREATE TABLE reuniones(
+    reunion_id integer not null primary key AUTO_INCREMENT,
+    reunion_fec_ini date not null,
+    reunion_fec_fin date not null,
+    reunion_titulo varchar(100) not null,
+	reunion_cuerpo text not null
+);
+
+DROP TABLE IF EXISTS imagenes_reunion;
+
+/**
+* una reunion, puede tener muchas imagenes, una imagen pertenece a una reunion.
+*/
+
+CREATE TABLE imagenes_reunion (
+    imagen_id integer not null primary key AUTO_INCREMENT,
+    imagen_path varchar(200) not null,
+    imagen_nombre varchar(200) not null,
+    imagen_reunion_id integer not null 
+);
+
+/**
+* foreign keys
+*/
+
+ALTER TABLE imagenes_reunion ADD CONSTRAINT `FK_imagenes_reunion_id_1` FOREIGN KEY (`imagen_reunion_id`) REFERENCES `reuniones` (`reunion_id`);
+
+/* FIN REUNIONES*/
+
+
+/* USUARIOS */
 CREATE TABLE usuarios(
     usuario_user varchar(20) not null primary key,
     usuario_pass varchar(32) not null
 );
-
-
+/* FIN USUARIOS */
