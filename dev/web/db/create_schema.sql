cb7ca360296d01f3edc15314271cfad90fa1057c
Facundo Scoccia <fanky10@gmail.com>
2013-02-19 08:26:17 -0300

    modificacion de tipo de datos de fecha inicio - fin

--- dev/web/db/create_schema.sql
+++ dev/web/db/create_schema.sql
@@ -47,8 +47,8 @@ DROP TABLE IF EXISTS reuniones;
 
 CREATE TABLE reuniones(
     reunion_id integer not null primary key AUTO_INCREMENT,
-    reunion_fec_ini date not null,
-    reunion_fec_fin date not null,
+    reunion_fec_ini datetime not null,
+    reunion_fec_fin datetime not null,
     reunion_titulo varchar(100) not null,
     reunion_cuerpo text not null,
     reunion_eliminada TINYINT(1) default 0
