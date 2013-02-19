ab4aeb40e887780e33d3667e564f04221d7a9284
Facundo Scoccia <fanky10@gmail.com>
2013-02-19 10:22:40 -0300

    agregar una reunion

---
+++ dev/test/datos/reuniones_test.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once ('../initTesting.php');
+require_once (WEB_ROOT_DIR . '/datos/reuniones.php');
+require_once (WEB_ROOT_DIR . '/datos/data.php');
+require_once (WEB_ROOT_DIR . '/entidades/reunion.php');
+require_once ('db_isolated_test_case.php');
+
+class ReunionesTest extends DatabaseIsolatedTestCase {
+    
+    private $reunionesRepository;
+    
+    public function __construct() {
+        parent::__construct("Imagenes Test");
+        $this->reunionesRepository = new DataReuniones();
+    }
+    
+    function testAddReunion() {
+        $idReunion = $this->agregaReunion();
+        $this->assertTrue(isset($idReunion));
+    }
+    
+    
+    private function agregaReunion(){
+        $reunion = new Reunion();
+        
+        $reunion->setCuerpo("Esto es un cuerpo");
+        $reunion->setTitulo("re title!");
+        $reunion->setFechaInicio(time());
+        $reunion->setFechaFin(time());
+
+        return $this->reunionesRepository->addReunion($reunion);
+    }
+
+}
+?>
--- dev/web/datos/reuniones.php
+++ dev/web/datos/reuniones.php
@@ -7,9 +7,29 @@
 @include_once ROOT_DIR . '/entidades/imagen.php';
 @include_once ROOT_DIR . '/util/utilidades.php';
 
-class DataNoticias extends Data implements ReunionesRepository {
+class DataReuniones extends Data implements ReunionesRepository {
     public function addReunion(Reunion $reunion) {
-        
+        $non_query = "insert into " . Reunion::$TABLE . " (reunion_fec_ini,reunion_fec_fin,reunion_titulo,reunion_cuerpo) 
+            values(?,?,?,?)";
+        $stmt = $this->prepareStmt($non_query);
+        $stmt->bind_param('ssss', $fecIni,$fecFin,$title, $body);
+        $fecIni = $reunion->getFechaInicio();
+        $fecFin = $reunion->getFechaFin();
+        $title = $reunion->getTitulo();
+        $cuerpo = $reunion->getCuerpo();
+        $body = $this->realEscapeString($cuerpo);
+
+
+        if (!$stmt->execute()) {
+            echo "addNoticia - Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+
+
+        /* close statement and connection */
+        $stmt->close();
+
+        $id = $this->getUltimoID(Reunion::$TABLE, Reunion::$COLUMN_ID);
+        return $id; //generated id
     }
 
     public function getCantidadReuniones() {
