6f8580ed3e36bb56d5e3d2528df669f2e3203beb
Facundo Scoccia <fanky10@gmail.com>
2013-02-19 12:23:36 -0300

    fix en el constructor de DataReuniones y formato

--- dev/web/datos/reuniones.php
+++ dev/web/datos/reuniones.php
@@ -8,22 +8,26 @@
 @include_once ROOT_DIR . '/util/utilidades.php';
 
 class DataReuniones extends Data implements ReunionesRepository {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
     public function addReunion(Reunion $reunion) {
         $non_query = "insert into " . Reunion::$TABLE . " (reunion_fec_ini,reunion_fec_fin,reunion_titulo,reunion_cuerpo) 
             values(?,?,?,?)";
         $stmt = $this->prepareStmt($non_query);
-        $stmt->bind_param('ssss', $fecIni,$fecFin,$title, $body);
+
+        $stmt->bind_param('ssss', $fecIni, $fecFin, $title, $body);
         $fecIni = $reunion->getFechaInicio();
         $fecFin = $reunion->getFechaFin();
         $title = $reunion->getTitulo();
         $cuerpo = $reunion->getCuerpo();
         $body = $this->realEscapeString($cuerpo);
 
-
         if (!$stmt->execute()) {
             echo "addNoticia - Execute failed: (" . $stmt->errno . ") " . $stmt->error;
         }
-
 
         /* close statement and connection */
         $stmt->close();
@@ -36,18 +40,18 @@ class DataReuniones extends Data implements ReunionesRepository {
         $query = "select r.reunion_id,r.reunion_titulo,r.reunion_cuerpo,r.reunion_fec_ini,r.reunion_fec_fin,r.reunion_eliminada
             from reuniones r 
             WHERE r.reunion_id= ?";
-        
+
         $stmt = $this->prepareStmt($query);
         $stmt->bind_param('i', $id);
         $stmt->execute();
-        
+
         $result = $stmt->get_result();
         $oReunion = null;
         while ($row = $result->fetch_assoc()) {
             $oReunion = $this->generaReunion($row);
         }
         $stmt->close();
-        
+
         return $oReunion;
     }
 
@@ -56,12 +60,12 @@ class DataReuniones extends Data implements ReunionesRepository {
             FROM reuniones r 
             WHERE r.reunion_eliminada=0
             ORDER BY r.reunion_fec_ini LIMIT ?";
-        
+
         $stmt = $this->prepareStmt($query);
         $stmt->bind_param('i', $limit);
         $stmt->execute();
         $result = $stmt->get_result();
-        
+
         $reunion_idx = 0;
         $vReuniones = array();
         while ($row = $result->fetch_assoc()) {
@@ -70,11 +74,11 @@ class DataReuniones extends Data implements ReunionesRepository {
             $reunion_idx = $reunion_idx + 1;
         }
         $stmt->close();
-        
+
         return $vReuniones;
     }
-    
-    private function generaReunion($row){
+
+    private function generaReunion($row) {
         $reunion = new Reunion();
         $reunion->setCuerpo($row['reunion_cuerpo']);
         $reunion->setEliminada($row['reunion_eliminada']);
@@ -82,22 +86,22 @@ class DataReuniones extends Data implements ReunionesRepository {
         $reunion->setFechaInicio($row['reunion_fec_ini']);
         $reunion->setId($row['reunion_id']);
         $reunion->setTitulo($row['reunion_titulo']);
-        
+
         return $reunion;
     }
 
     public function editarReunion(Reunion $reunion) {
         $non_query = "update " . Reunion::$TABLE . " set reunion_titulo=?, reunion_cuerpo=?, reunion_eliminada=?,reunion_fec_ini=?,reunion_fec_fin=? where reunion_id=?";
         $stmt = $this->prepareStmt($non_query);
-        $stmt->bind_param('ssissi', $title, $body, $eliminada,$fecIni,$fecFin, $id);
-        
+        $stmt->bind_param('ssissi', $title, $body, $eliminada, $fecIni, $fecFin, $id);
+
         $fecIni = $reunion->getFechaInicio();
         $fecFin = $reunion->getFechaFin();
         $title = $reunion->getTitulo();
         $eliminada = $reunion->getEliminada();
         $cuerpo = $reunion->getCuerpo();
         $body = $this->realEscapeString($cuerpo);
-        
+
         $id = $reunion->getId();
 
         if (!$stmt->execute()) {
@@ -106,6 +110,7 @@ class DataReuniones extends Data implements ReunionesRepository {
         /* close statement and connection */
         $stmt->close();
     }
-            
+
 }
+
 ?>
