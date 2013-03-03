6335bd636ac16518a1b3303079853feb81dc8a30
Facundo Scoccia <fanky10@gmail.com>
2013-03-02 21:27:35 -0300

    ahora el calendario funciona bien y recibe como parametros una lista de objetos mocked

--- dev/web/reuniones.php
+++ dev/web/reuniones.php
@@ -24,39 +24,84 @@
         <link rel="stylesheet" href="stylesheets/prettyPhoto.css">
         
 		
-		<!-- Include CSS for JQuery Frontier Calendar plugin (Required for calendar plugin) -->
-		<link rel="stylesheet" type="text/css" href="stylesheets/frontierCalendar/jquery-frontier-cal-1.3.2.css" />
-		<link rel="stylesheet" type="text/css" href="stylesheets/colorpicker/colorpicker.css" />
-		<link rel="stylesheet" type="text/css" href="stylesheets/jquery-ui/smoothness/jquery-ui-1.8.1.custom.css" />
-		
-		<!--Include JQuery Core (Required for calendar plugin)-->
-		<script type="text/javascript" src="javascripts/jquery-core/jquery-1.4.2-ie-fix.min.js"></script>
-		<script type="text/javascript" src="javascripts/jquery-ui/smoothness/jquery-ui-1.8.1.custom.min.js"></script>
-		<script type="text/javascript" src="javascripts/colorpicker/colorpicker.js"></script>
-		<script type="text/javascript" src="javascripts/jquery-qtip-1.0.0-rc3140944/jquery.qtip-1.0.js"></script>
-		<script type="text/javascript" src="javascripts/lib/jshashtable-2.1.js"></script>
-		<script type="text/javascript" src="javascripts/frontierCalendar/jquery-frontier-cal-1.3.2.min.js"></script>
-        
+        <!-- Include CSS for JQuery Frontier Calendar plugin (Required for calendar plugin) -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/frontierCalendar/jquery-frontier-cal-1.3.2.css" />
+        <link rel="stylesheet" type="text/css" href="stylesheets/colorpicker/colorpicker.css" />
+        <link rel="stylesheet" type="text/css" href="stylesheets/jquery-ui/smoothness/jquery-ui-1.8.1.custom.css" />
+
+        <!--Include JQuery Core (Required for calendar plugin)-->
+        <script type="text/javascript" src="javascripts/jquery-core/jquery-1.4.2-ie-fix.min.js"></script>
+        <script type="text/javascript" src="javascripts/jquery-ui/smoothness/jquery-ui-1.8.1.custom.min.js"></script>
+        <script type="text/javascript" src="javascripts/colorpicker/colorpicker.js"></script>
+        <script type="text/javascript" src="javascripts/jquery-qtip-1.0.0-rc3140944/jquery.qtip-1.0.js"></script>
+        <script type="text/javascript" src="javascripts/lib/jshashtable-2.1.js"></script>
+        <script type="text/javascript" src="javascripts/frontierCalendar/jquery-frontier-cal-1.3.2.min.js"></script>
+        <script type="text/javascript" >
+            /**
+                * Initialize with current year and date. Returns reference to plugin object.
+                */
+               $(document).ready(
+                function() {
+                    var jfcalplugin = $("#reunionesCal").jFrontierCal({
+                        date: new Date(),
+                        dragAndDropEnabled: false
+                    }).data("plugin");
+                    try{
+                        /**
+                         * http://library.osu.edu/inc/frontierCalendar/calendar.html#AddAgendaItem
+                         */
+                        var dataList = createCalendarData();
+                        $.each(dataList,function(idx,item){
+                            jfcalplugin.addAgendaItem(
+                            "#reunionesCal",
+                            item.titulo,
+                            item.fechaInicio,
+                            item.fechaFin,
+                            item.allDay,
+                            item.data,
+                            item.displayProp
+                            );
+                        });
+                    } catch (e) {
+                            alert("Added not agenda",e.message);
+                    } 
+
+                });
+                function createCalendarData(){
+                    var result = new Array();
+                    var object = {
+                        titulo:"Test Som sheeet jeje",
+                        fechaInicio:new Date(2013,02,05,0,0,0,0),
+                        fechaFin: new Date(2013,02,08,0,0,0,0),
+                        allDay:true,
+                        data:{url:"http://google.com"},
+                        displayProp:{backgroundColor:null,foregroundColor:null}
+                        
+                    }
+                    result.push(object);
+                    return result;
+                }
+        </script>
 		
 
         <!-- Author -->
         <link type="text/plain" rel="author" href="humans.txt" />
 
-		<style type='text/css'>
-		
-			body {
-				margin-top: 40px;
-				text-align: center;
-				font-size: 14px;
-				font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-				}
-		
-			#calendar {
-				width: 750px;
-				margin: 0 auto;
-				}
-		
-		</style>
+        <style type='text/css'>
+
+                body {
+                        margin-top: 40px;
+                        text-align: center;
+                        font-size: 14px;
+                        font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+                        }
+
+                #calendar {
+                        width: 750px;
+                        margin: 0 auto;
+                        }
+
+        </style>
         
     </head>
     <body>
@@ -73,16 +118,28 @@
         include_once 'navigate.php'
         ?>
         
-        <?php include_once 'calendar_init.php'; ?>
 
         <!-- Three-up Content Blocks -->
         <div class="content">
-        	<div class="row">
-        		<div class="twelve columns">
-              		<?php include_once 'calendar.php'; ?>  
+            <div class="row">
+                <div class="twelve columns">
+                    <br><br>
+                    <div id="toolbar" class="ui-widget-header ui-corner-all" style="padding:3px; vertical-align: middle; white-space:nowrap; overflow: hidden;">
+                        <button id="BtnPreviousMonth">Mes Anterior</button>
+                        <button id="BtnNextMonth">Mes Siguiente</button>
+                    </div>
+        
+                    <br>
+        
+                    <!--
+                    You can use pixel widths or percentages. Calendar will auto resize all sub elements.
+                    Height will be calculated by aspect ratio. Basically all day cells will be as tall
+                    as they are wide.
+                    -->
+                    <div id="reunionesCal"></div>
                 </div>
-        	</div>
-		</div>
+            </div>
+        </div>
 
         <!-- Footer -->
         <?php include_once 'footer.php'; ?> 
