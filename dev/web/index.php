7db9005f0203aafe1686cdb8e5314f0449236fc5
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2013-03-08 20:01:57 -0300

    arreglo sponsors

--- dev/web/index.php
+++ dev/web/index.php
@@ -87,23 +87,7 @@
 
                     <br>
                     <br>
-                    <div class="eight columns">
-                        <div class="four columns sponsor-wrapper">
-                            <div class="table">
-                                <ul id="sponsors"> 
-                                    <li><a class="redTelesaludWhite" href="#"></a></li> 
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="four columns sponsor-wrapper">
-                            <div class="table">
-                                <ul id="sponsors"> 
-                                    <li><a class="bvs" href="#"></a></li> 
-                                </ul>
-                            </div>
-
-                        </div>
-                    </div>
+                    <?php include_once 'sponsors_destacados.php'?>
 
                 </div>
 
