c59fc90771481ed34b3fa9d41a423e5015bc2424
Facundo Scoccia <fanky10@gmail.com>
2013-05-26 19:20:34 -0300

    icono para ver documento fix

--- dev/web/admin/documentos.php
+++ dev/web/admin/documentos.php
@@ -79,7 +79,7 @@ $oDocumento = new Documento();
 
                             echo '<div class = "four columns" align = "center">';
                             $linkDownload = ROOT_URL . "/" . $oDocumento->getPath() . "/" . $oDocumento->getNombreArchivo();
-                            echo '<a href="' . $linkDownload . '"><img src="../images/soft-scraps-download-icon.png" alt="Editar" /></a>';
+                            echo '<a href="' . $linkDownload . '"><img src="'.ROOT_URL . '/images/soft-scraps-download-icon.png" alt="Editar" /></a>';
                             echo '</div>';
 
                             echo '<div class="two columns" align="center">';
